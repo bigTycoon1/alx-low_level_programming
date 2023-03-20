0b04be0ffffc4a199b5699d846d6c66ae84e7641
bigTycoon1 <mztatycoon@gmail.com>
2023-03-20 04:56:25 -0700

    prints all possible different combinations of three digits.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,12 +20,12 @@ int main(void)
 					putchar(x);
 					putchar(y);
 					putchar(z);
-					
-						if (x < 55 || y < 56 || z < 57)
-						{
-							putchar(',');
-							putchar(' ');
-						}
+
+					if (x < 55 || y < 56 || z < 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
