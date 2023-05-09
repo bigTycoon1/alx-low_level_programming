ad198fa39a4e3ef4d692b82e76fd5b19f014dbb3
bigTycoon1 <mztatycoon@gmail.com>
2023-05-09 04:43:05 -0700

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,7 @@ int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
+int get_endianness(void);
+
 
 #endif /* MAIN_H */
